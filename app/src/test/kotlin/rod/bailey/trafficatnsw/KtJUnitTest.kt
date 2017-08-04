72a21ee49a377bc090ee873222742fee292f155c
rodbailey <rodney.p.bailey@gmail.com>
2017-08-04 17:50:27 +1000

    Change gradle to enable Kotlin for instrumented and un-instrumented tests

---
+++ app/src/test/kotlin/rod/bailey/trafficatnsw/KtJUnitTest.kt
@@ -0,0 +1,19 @@
+package rod.bailey.trafficatnsw
+
+import org.junit.Test
+
+/**
+ * Created by rodbailey on 4/8/17.
+ */
+class KtJUnitTest {
+
+	@Test
+	fun myTest() {
+		//assertTrue(2 == 2);
+	}
+
+	@Test
+	fun myTest2() {
+		//assertTrue(3 == 3);
+	}
+}
