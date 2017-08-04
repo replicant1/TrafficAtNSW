78628f702332710f53d350d26939d54d191a8e0c
rodbailey <rodney.p.bailey@gmail.com>
2017-08-04 22:01:12 +1000

    Introduced empty, TestMainActivity that is not an @EActivity - this makes instrumented tests work OK.

--- app/src/androidTest/kotlin/rod/bailey/trafficatnsw/inst/test/KtInstrumentedTest.kt
+++ app/src/androidTest/kotlin/rod/bailey/trafficatnsw/inst/test/KtInstrumentedTest.kt
@@ -1,21 +1,26 @@
 package rod.bailey.trafficatnsw.inst.test
 
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import rod.bailey.trafficatnsw.app.TestMainActivity
 import kotlin.test.assertTrue
 
 /**
  * Instrumentation test, which will execute on an Android device.
  * Fri 4 Aug 6pm - this works until we insert the activity rule
  * Then it fails because of: https://discuss.kotlinlang.org/t/class-reflection-get-incorrect-class-name/3920
- * A bug in Kotlin reflection. A solution may be to remove @EActivity from MainActivity
+ * A bug in Kotlin reflection. A solution may be to remove @EActivity from MainActivity.
+ * Test : when I changed the launch activity from MainActivity to the empty non-@EActivity TestMainActivity,
+ * it worked.
  */
-//@LargeTest
-//@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class KtInstrumentedTest {
 
-//	@Rule @JvmField
-//	var activity:@JvmField
-//	var activity: ActivityTestRule<MainActivity_> = ActivityTestRule<MainActivity_>(MainActivity_::class.java)
+	@Rule @JvmField
+	var activity: ActivityTestRule<TestMainActivity> = ActivityTestRule<TestMainActivity>(TestMainActivity::class.java)
 
 	@Test
 	fun testMe() {
